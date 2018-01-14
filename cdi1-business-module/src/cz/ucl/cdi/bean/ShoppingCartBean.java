97566d579046d10af1928bc08aff9d5591d534c8
Ondrej Fialka <ondrej.fialka@unicorncollege.cz>
2018-01-14 17:52:28 +0100

    changed project to war instead of ear

---
+++ cdi1-business-module/src/cz/ucl/cdi/bean/ShoppingCartBean.java
@@ -0,0 +1,37 @@
+package cz.ucl.cdi.bean;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cz.ucl.cdi.annotation.CreditCard;
+import cz.ucl.cdi.annotation.Monitored;
+import cz.ucl.cdi.annotation.Visa;
+import cz.ucl.cdi.annotation.WireTransfer;
+
+@SessionScoped
+@Named("cart")
+public class ShoppingCartBean implements ShoppingCart, Serializable {
+    @Inject
+    private Payment payment;
+
+    @Inject
+    @CreditCard
+    @Visa
+    private PaymentProcessor ccPaymentProcessor;
+    
+    @Inject
+    @WireTransfer
+    private PaymentProcessor transferPaymentProcessor;
+
+    @Monitored
+    public void checkout() {
+        if (payment.getType() == PaymentType.CREDIT_CARD) {
+            ccPaymentProcessor.pay(payment, 10.0);            
+        } else if(payment.getType() == PaymentType.WIRE_TRANSFER){  
+        	transferPaymentProcessor.pay(payment, 10.0);
+        }
+    }
+}
